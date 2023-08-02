fa526262e11f721f3b475ca28195ba222baeaa41
Debojit Kumar Das <debojit2997@gmail.com>
2023-08-02 11:04:53 +0530

    Updated the mandelbrot set code

--- 2D_graphics/mandelbrot_set/main.cpp
+++ 2D_graphics/mandelbrot_set/main.cpp
@@ -1,14 +1,93 @@
 #include <SFML/Graphics.hpp>
-#include <cstdio>
+// #include <immintrin.h>
 
 #define WIDTH  1920
 #define HEIGHT 1080
+
+int max_iter = 64;
+double min_re = -2.5, max_re = 1.0;
+double min_im = -1.0, max_im = 1.0;
+double zoom = 1.0;
 
 sf::Color linear_interpolation(const sf::Color& v, const sf::Color& u, double a) {
     auto const b = 1 - a;
 
     return sf::Color(b * v.r + a * u.r, b * v.g + a * u.g, b * v.b + a * u.b);
 }
+
+void naive_method(sf::RenderWindow *window, sf::Texture *texture, sf::Sprite *sprite, sf::Text *text, sf::Image *image) {
+    int iter;
+
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            double cr = min_re + (max_re - min_re) * x / WIDTH;
+            double ci = min_im + (max_im - min_im) * y / HEIGHT;
+            double re = 0, im = 0;
+
+            for (iter = 0; iter < max_iter; ++iter) {
+                double tr = re * re - im * im + cr;
+
+                im = 2 * re * im + ci;
+                re = tr;
+
+                if (re * re + im * im > 4)
+                    break;
+            }
+
+            static const std::vector<sf::Color> colors {
+                {0, 7, 100},
+                {32, 107, 203},
+                {237, 255, 255},
+                {255, 170, 0},
+                {0, 2, 0}
+            };
+
+            static const auto max_color = colors.size() - 1;
+
+            if (iter == max_iter)
+                iter = 0;
+
+            double mu = 1.0 * iter / max_iter;
+
+            mu *= max_color;
+            auto i_mu = static_cast<size_t>(mu);
+            auto color1 = colors[i_mu];
+            auto color2 = colors[std::min(i_mu + 1, max_color)];
+            sf::Color c = linear_interpolation(color1, color2, mu - i_mu);
+
+            image->setPixel(x, y, sf::Color(c));
+        }
+
+        texture->loadFromImage(*image);
+        sprite->setTexture(*texture);
+        window->draw(*sprite);
+
+        char str[100];
+
+        sprintf(str, "max_iter: %d\nzoom: x%2.2lf", max_iter, zoom);
+        text->setString(str);
+        window->draw(*text);
+    }
+}
+
+/* Optimizing the code using compiler intrinsics */
+/*
+void using_comp_int(sf::RenderWindow *window, sf::Texture *texture, sf::Sprite *sprite, sf::Text *text, sf::Image *image) {
+    int iter;
+
+    __m256d _a, _b, _two, _four, _mask1;
+    __m256d _zr, _zi, _zr2, _cr, _ci;
+    __m256d _x_pos_offsets, _x_pos, _x_scale, _jump;
+    __m256i _one, _c, _n, _iterations, _mask2;
+
+    _one = _mm256_set1_epi64x(1);
+    _two = _mm256_set1_pd(2.0);
+    _four = _mm256_set1_pd(4.0);
+    _iterations = _mm256_set1_epi64x(iter);
+
+    _x_scale = _mm256_set1_pd(x_scale)
+}
+*/
 
 int main() {
 
@@ -25,11 +104,6 @@ int main() {
     text.setFillColor(sf::Color::White);
 
     image.create(WIDTH, HEIGHT);
-
-    int max_iter = 128;
-    double min_re = -2.5, max_re = 1.0;
-    double min_im = -1.0, max_im = 1.0;
-    double zoom = 1.0;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -57,14 +131,14 @@ int main() {
 
                 // left click zoom-in
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    zoom_x(5);
-                    zoom *= 5;
+                    zoom_x(2);
+                    zoom *= 2;
                 }
 
                 // right click zoom-out
                 if (event.mouseButton.button == sf::Mouse::Right) {
-                    zoom_x(1.0 / 5);
-                    zoom /= 5.0;
+                    zoom_x(1.0 / 2);
+                    zoom /= 2.0;
                 }
             }
 
@@ -72,8 +146,8 @@ int main() {
             if (event.type == sf::Event::KeyPressed) {
 
                 // deltas
-                double w = (max_re - min_re) * 0.3;
-                double h = (max_im - min_im) * 0.3;
+                double w = (max_re - min_re) * 0.1;
+                double h = (max_im - min_im) * 0.1;
 
                 // move left
                 if (event.key.code == sf::Keyboard::Left) {
@@ -101,6 +175,7 @@ int main() {
             }
 
             // set iteration levels
+            /*
             if (event.type == sf::Event::MouseWheelScrolled) {
                 if (event.MouseWheelScrolled != 0) {
                     if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
@@ -114,65 +189,19 @@ int main() {
                     }
                 }
             }
+            */
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::U)
+                    max_iter *= 2;
+                if (event.key.code == sf::Keyboard::P)
+                    max_iter /= 2;
+            }
         }
+
         window.clear();
 
-        // #pragma omp parallel for
-        for (int y = 0; y < HEIGHT; ++y) {
-            for (int x = 0; x < WIDTH; ++x) {
-                double cr = min_re + (max_re - min_re) * x / WIDTH;
-                double ci = min_im + (max_im - min_im) * y / HEIGHT;
-                double re = 0, im = 0;
-                int iter;
+        naive_method(&window, &texture, &sprite, &text, &image);
 
-                for (iter = 0; iter < max_iter; ++iter) {
-                    double tr = re * re - im * im + cr;
-
-                    im = 2 * re * im + ci;
-                    re = tr;
-
-                    if (re * re + im * im > 4)
-                        break;
-                }
-
-                // int r = 1.0 * (max_iter - iter) / max_iter * 0xff;
-                // int g = r, b = r;
-
-                // color pallet similar to wikipedia
-                static const std::vector<sf::Color> colors {
-                    {0, 7, 100},
-                    {32, 107, 203},
-                    {237, 255, 255},
-                    {255, 170, 0},
-                    {0, 2, 0}
-                };
-
-                static const auto max_color = colors.size() - 1;
-
-                if (iter == max_iter)
-                    iter = 0;
-
-                double mu = 1.0 * iter / max_iter;
-
-                mu *= max_color;
-                auto i_mu = static_cast<size_t>(mu);
-                auto color1 = colors[i_mu];
-                auto color2 = colors[std::min(i_mu + 1, max_color)];
-                sf::Color c = linear_interpolation(color1, color2, mu - i_mu);
-
-                image.setPixel(x, y, sf::Color(c));
-            }
-
-            texture.loadFromImage(image);
-            sprite.setTexture(texture);
-            window.draw(sprite);
-
-            char str[100];
-
-            sprintf(str, "max_iter: %d\nzoom: x%2.2lf", max_iter, zoom);
-            text.setString(str);
-            window.draw(text);
-        }
         window.display();
     }
 
